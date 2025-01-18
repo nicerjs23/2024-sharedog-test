27df5432fcb7294e3f3af2d4f6c1c974e3280584
nicerjs23 <nicerjs23@gmail.com>
2025-01-18 21:28:40 +0900

    Feat: 초기세팅

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,28 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { ThemeProvider } from "styled-components";
+import { theme } from "@styles/theme";
+import GlobalStyle from "@styles/global";
 
+import { RouterProvider } from "react-router-dom";
+import { router } from "./router";
+import { useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0)
+  useEffect(() => {
+    const setVh = () => {
+      const vh = window.innerHeight * 0.01;
+      document.documentElement.style.setProperty("--vh", `${vh}px`);
+    };
 
+    setVh(); // 초기값 설정
+    window.addEventListener("resize", setVh); // 창 크기 변경 시 재계산
+
+    return () => window.removeEventListener("resize", setVh); // 이벤트 해제
+  }, []);
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <ThemeProvider theme={theme}>
+      <GlobalStyle />
+      <RouterProvider router={router} />
+    </ThemeProvider>
+  );
 }
 
-export default App
+export default App;
---
+++ src/pages/testPage/StartPage.jsx
@@ -0,0 +1,5 @@
+import * as S from "./StartPage.styled";
+
+export const StartPage = () => {
+  return <S.MainWrapper>나눠주개 테스트 시작페이지</S.MainWrapper>;
+};
