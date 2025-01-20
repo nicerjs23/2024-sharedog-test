ab3e2ada6068231fa175d9ddcc2b3ddfb6e2a0fa
nicerjs23 <nicerjs23@gmail.com>
2025-01-20 21:48:33 +0900

    Fix: 결과 공유하기 수정완

--- src/pages/testPage/TestResultNoPage.jsx
+++ src/pages/testPage/TestResultNoPage.jsx
@@ -7,10 +7,11 @@ import useShare from "@hooks/useShare";
 
 export const TestResultNoPage = () => {
   const { goTo } = useCustomNavigate();
+  const startUrl = `${window.location.origin}/`; // 시작화면의 URL을 고정시킴
   const { handleShare } = useShare({
     title: "우리집 강아지는 헌혈견이 될 수 있을까?",
     text: "강아지의 헌혈 가능 여부를 테스트해보세요!",
-    url: window.location.href,
+    url: startUrl,
   });
 
   return (
